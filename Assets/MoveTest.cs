7bba7e12f97ce3a0522c5a2367d5341972db981c
Sebastian Mäki <sebastian.maki@gmail.com>
2021-04-01 11:09:17 +0300

    FightingPit scene and associated assets

---
+++ Assets/MoveTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+	[RequireComponent(typeof (ThirdPersonCharacter))]
+	[RequireComponent(typeof (AICharacterControl))]
+	public class MoveTest : MonoBehaviour
+	{
+		public AICharacterControl character { get; private set; } // the character we are controlling
+		// Start is called before the first frame update
+		void Start()
+		{
+		    character = GetComponent<AICharacterControl>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (Input.GetMouseButton(0)) {
+				HandleInput();
+			}
+		}
+		
+		void HandleInput () {
+			Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(inputRay, out hit)) {
+				moveTo(hit.point);
+			}
+		}
+		void moveTo (Vector3 position) {
+			position = transform.InverseTransformPoint(position);
+			Debug.Log(position);
+		}
+	}
+}
